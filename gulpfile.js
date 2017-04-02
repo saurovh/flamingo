fe5fb4439fe99dad8d46639877ccf3068873c2df
Fisherman <sadequl@ipvision-inc.com>
2017-04-02 19:40:03 +0600

    removing jshint and adding eslint

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
+    eslint = require('gulp-eslint'),
     header  = require('gulp-header'),
     rename = require('gulp-rename'),
     cssnano = require('gulp-cssnano'),
@@ -40,8 +40,8 @@ gulp.task('css', function () {
 gulp.task('js',function(){
   gulp.src('src/js/scripts.js')
     .pipe(sourcemaps.init())
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
+    .pipe(eslint('.eslintrc.json'))
+    .pipe(eslint.format())
     .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('app/assets/js'))
     .pipe(uglify())
